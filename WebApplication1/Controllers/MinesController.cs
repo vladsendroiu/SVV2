6e1a561ec289ec0520f9c32a1ddc9b3b2cb085f1
VladSendroiu <vlad_sendroiu@yahoo.com>
2017-05-02 20:52:54 +0300

    first commit

---
+++ WebApplication1/Controllers/MinesController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Microsoft.Ajax.Utilities;
+using Microsoft.AspNet.Identity;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    [Authorize]
+    public class MinesController : Controller
+    {
+        ApplicationDbContext dbContext = new ApplicationDbContext();
+        // GET: Mines
+        public ActionResult Index()
+        {
+            var userId = this.User.Identity.GetUserId();
+            var user = dbContext.Users.Find(userId);
+            var city = user.Cities.First();
+            this.UpdteResources(city);
+            return View(city);
+        }
+        public ActionResult Details(int mineId)
+        {
+            var userId = this.User.Identity.GetUserId();
+            var user = dbContext.Users.Find(userId);
+            var city = user.Cities.First();
+            
+            var mine = dbContext.Mines.Find(mineId);
+            return View(mine);
+        }
+        private void UpdteResources(City city)
+        {
+            var start = DateTime.Now;
+            foreach (var res in city.Resources)
+            {
+                foreach (var mine in city.Mines)
+                {
+                    if (mine.Type == res.Type)
+                    {
+                        res.Value += mine.GetProductionPerHour() * (start - res.LastUpdate).TotalHours;
+                    }
+                }
+                res.LastUpdate = start;
+            }
+            dbContext.SaveChanges();
+
+        }
+    }
+}
---
+++ WebApplication1/Scripts/myscripts/mines.js
@@ -0,0 +1,28 @@
+﻿$(document).ready(function () {
+
+    var updateResources = function () {
+        updateResource("Clay");
+        updateResource("Wheat");
+        updateResource("Iron");
+        updateResource("Wood");
+    };
+    var updateResource = function (resorceName) {
+        var start = new Date();
+        var currentProduction = 0;
+        var currentValue = parseFloat($(".res-value." + resorceName).text());
+        //console.log(currentValue);
+        var lastUpdate = Date.parse($(".res-update." + resorceName).text());
+        //console.log(lastUpdate );
+        var mines = $(".mines").find("." + resorceName);
+        $.each(mines, function (index, value)
+        {
+            currentProduction += parseInt($(value).find(".hourProduction").text());
+        }
+        );
+        console.log(currentProduction);
+        var nextValue = (currentValue + ((start.getTime() - lastUpdate) / 1000 / 60 / 60) * currentProduction).toFixed(4);
+        $(".res-value." + resorceName).text(nextValue);
+        $(".res-update." + resorceName).text(start.strftime("%m/%d/%Y %H:%M:%S %p"));
+    };
+    setInterval(updateResources, 1000);
+});
