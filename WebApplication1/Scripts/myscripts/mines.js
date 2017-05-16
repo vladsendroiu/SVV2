b871bc95f058e3eb00b91bb53ff901dfaf66b901
VladSendroiu <vlad_sendroiu@yahoo.com>
2017-05-16 20:59:32 +0300

    16Mai

--- WebApplication1/Scripts/myscripts/mines.js
+++ WebApplication1/Scripts/myscripts/mines.js
@@ -10,7 +10,7 @@
         var start = new Date();
         var currentProduction = 0;
         var currentValue = parseFloat($(".res-value." + resorceName).text());
-        //console.log(currentValue);
+        console.log(currentValue);
         var lastUpdate = Date.parse($(".res-update." + resorceName).text());
         //console.log(lastUpdate );
         var mines = $(".mines").find("." + resorceName);
@@ -19,10 +19,25 @@
             currentProduction += parseInt($(value).find(".hourProduction").text());
         }
         );
-        console.log(currentProduction);
+       // console.log(currentProduction);
         var nextValue = (currentValue + ((start.getTime() - lastUpdate) / 1000 / 60 / 60) * currentProduction).toFixed(4);
         $(".res-value." + resorceName).text(nextValue);
-        $(".res-update." + resorceName).text(start.strftime("%m/%d/%Y %H:%M:%S %p"));
+        $(".res-update." + resorceName).text(start.strftime("%m/%d/%Y %H:%M:%S"));
     };
     setInterval(updateResources, 1000);
+    //afis
+    var getMineDetailsHTML = function (mineId) {
+        $('#mine-details-container > .content').empty();
+        $('#mine-details-container > .content').load("/Mines/Details?mineId=" + mineId);
+        $('#mine-details-container').addClass('show');
+    };
+    $('.mine-details-btn').click(function (e)
+    {
+        var mineId = $(this).data('mine-id');
+        getMineDetailsHTML(mineId);
+    })
+    //inchide
+    $('#mine-details-container > .close-btn').click(function () {
+        $('#mine-details-container').removeClass('show');
+    })
 });
